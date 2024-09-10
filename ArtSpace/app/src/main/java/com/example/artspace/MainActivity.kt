e095d8a08de4d15892db9b2cac1811a106e5c7bb
Ami Aram <amiaram91@gmail.com>
2024-09-10 16:21:15 -0300

    finished app layout

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,8 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,35 +52,107 @@ fun ArtSpace(modifier: Modifier = Modifier) {
     Column (
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     )
     {
-        val image = painterResource(id = R.drawable._04ddb287758d86296047b8ac3de2477)
-//        val image = painterResource(id = R.drawable._579667fb1cacc2f8364530865910e57)
-
-        Surface (
-            shadowElevation = 8.dp,
-            modifier = Modifier
-                .weight(0.5f)
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 32.dp, vertical = 32.dp
-                )
-        ){
-            Image(
-                painter = image,
-                contentDescription = null,
+        ArtWall(
+            imageResourceId = R.drawable._04ddb287758d86296047b8ac3de2477,
+            contentDescriptionResourceId = R.string.psyducks_content_description
+        )
+        ArtDescriptor(
+            titleResourceId = R.string.psyducks_artwork_title,
+            artistResourceId = R.string.psyducks_artwork_artist,
+            yearResourceId = R.string.psyducks_artwork_year
+        )
+        Spacer(modifier = modifier.height(50.dp))
+    }
+    Column (
+        verticalArrangement = Arrangement.Bottom,
+        modifier = modifier.fillMaxSize()
+    )
+    {
+        Row {
+            Button (
+                onClick = { /*TODO*/ },
                 modifier = Modifier
-                    .padding(32.dp)
-            )
+                    .weight(0.5f)
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 16.dp
+                    )
+            ) {
+                Text(text = stringResource(R.string.previous_button))
+            }
+            Button (
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .weight(0.5f)
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 16.dp
+                    )
+            ) {
+                Text(text = stringResource(R.string.next_button))
+            }
         }
+    }
+}
 
-        Surface (
+@Composable
+fun ArtWall (
+    imageResourceId: Int,
+    contentDescriptionResourceId: Int,
+) {
+    Surface (
+        shadowElevation = 8.dp,
+        modifier = Modifier
+            .height(500.dp)
+            .fillMaxWidth()
+            .padding(
+                horizontal = 32.dp, vertical = 60.dp
+            )
+    ){
+        Image(
+            painter = painterResource(imageResourceId),
+            contentDescription = stringResource(contentDescriptionResourceId),
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Red)
-                .weight(0.5f)
-        ){
-            Text(text = "Nome da obra")
+                .padding(32.dp)
+        )
+    }
+}
+
+@Composable
+fun ArtDescriptor (
+    titleResourceId: Int,
+    artistResourceId: Int,
+    yearResourceId: Int
+) {
+    Surface (
+        color = Color.LightGray,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 16.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(24.dp)
+        )
+        {
+            Text(
+                fontWeight = FontWeight.Light,
+                fontSize = 24.sp,
+                text = stringResource(titleResourceId),
+                textAlign = TextAlign.Start,
+            )
+            Row {
+                Text(
+                    text = stringResource(artistResourceId),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = "(${stringResource(yearResourceId)})"
+                )
+            }
         }
     }
 }
